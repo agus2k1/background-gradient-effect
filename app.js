4a2bded26bcee87f82aa689da0bd09f21d0d2b68
West <agus2kcoin@outlook.com>
2023-03-22 21:50:16 -0300

    Feature sketch

---
+++ app.js
@@ -0,0 +1,58 @@
+import './main.css';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import fragment from './shaders/fragment.glsl.js';
+import vertex from './shaders/vertex.glsl.js';
+
+export default class Sketch {
+  constructor() {
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer.setClearColor(0xeeeeee, 1);
+    document.getElementById('container').appendChild(this.renderer.domElement);
+
+    this.camera = new THREE.PerspectiveCamera(
+      70,
+      window.innerWidth / window.innerHeight,
+      0.001,
+      1000
+    );
+    this.camera.position.set(0, 0, 1);
+    this.scene = new THREE.Scene();
+
+    this.move = 0;
+    this.time = 0;
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+
+    this.addMesh();
+    this.render();
+  }
+
+  addMesh() {
+    this.material = new THREE.ShaderMaterial({
+      extensions: {
+        derivatives: '#extension GL_OES_standard_derivatives : enable',
+      },
+      uniforms: {
+        time: { type: 'f', value: 0 },
+      },
+      fragmentShader: fragment,
+      vertexShader: vertex,
+      side: THREE.DoubleSide,
+      wireframe: true,
+    });
+    this.geometry = new THREE.PlaneGeometry(1, 1, 300, 300);
+
+    this.plane = new THREE.Mesh(this.geometry, this.material);
+    this.scene.add(this.plane);
+  }
+
+  render() {
+    this.time += 0.0005;
+    this.material.uniforms.time.value = this.time;
+    this.renderer.render(this.scene, this.camera);
+    window.requestAnimationFrame(this.render.bind(this));
+  }
+}
+
+new Sketch();
