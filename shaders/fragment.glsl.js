7edcedf2947cc779d9e79ae41614e9749f63092c
West <agus2kcoin@outlook.com>
2023-03-23 17:18:20 -0300

    Add color gradient

--- shaders/fragment.glsl.js
+++ shaders/fragment.glsl.js
@@ -4,7 +4,7 @@ const fragmentShader = `
 
     void main() {
         gl_FragColor = vec4(vUv, 0., 1.);
-        // gl_FragColor = vec4(vColor, 1.);
+        gl_FragColor = vec4(vColor, 1.);
     }
 `;
 
